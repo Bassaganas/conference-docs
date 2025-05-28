a82d43ee133b7daedd58dde9d3adb509be99e1e5
bassagan <bassagan@users.noreply.github.com>
2025-05-28 08:58:55 +0000

    deploy: fe1ac5b9b51b625dbc46e9cced6928762d6bd452

--- assets/js/4d18bf67.4cd3f644.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkconference_docs=self.webpackChunkconference_docs||[]).push([[590],{7846:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/testus-patronus/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- assets/js/acecf23e.93d9ba57.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkconference_docs=self.webpackChunkconference_docs||[]).push([[903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/testus-patronus/blog","blogTitle":"Blog","authorsListPath":"/testus-patronus/blog/authors"}')}}]);
