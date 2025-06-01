d9c052d39c3f1453d2e271fc648adb4fcc525894
paula.bassaganas@boehringer-ingelheim.com <paula.bassaganas@boehringer-ingelheim.com>
2025-06-01 22:46:08 +0200

    chores: minor improvements

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -68,6 +68,11 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+    },
     navbar: {
       title: 'Testus Patronus',
       logo: {
